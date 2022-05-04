d50c3e3a00aee4d6ee87b499b4c11cbb674735d6
nehakumariWellnest <104764857+nehakumariWellnest@users.noreply.github.com>
2022-05-04 15:52:09 +0530

    create component and define the path

--- logitifieBootstrap/src/app/app-routing.module.ts
+++ logitifieBootstrap/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogitifieFormComponent } from './logitifie-form/logitifie-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'logitifieForm',component:LogitifieFormComponent},
+  {path:'',redirectTo:'logitifieForm',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
