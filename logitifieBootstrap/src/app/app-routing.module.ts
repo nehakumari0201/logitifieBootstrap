931d3d4b72b031909604c549ab81caf5620cd0c2
nehakumariWellnest <104764857+nehakumariWellnest@users.noreply.github.com>
2022-05-06 17:31:03 +0530

    create side nav

--- logitifieBootstrap/src/app/app-routing.module.ts
+++ logitifieBootstrap/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
+import { SidenavComponent } from './sidenav/sidenav.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LogitifieFormComponent } from './logitifie-form/logitifie-form.component';
 
 const routes: Routes = [
   {path:'logitifieForm',component:LogitifieFormComponent},
-  {path:'',redirectTo:'logitifieForm',pathMatch:'full'}
+  {path:'',redirectTo:'logitifieForm',pathMatch:'full'},
+  {path:'sidenav',component:SidenavComponent}
 ];
 
 @NgModule({
